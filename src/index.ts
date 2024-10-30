e003e4ee7876f2d84786c70745e4399e8b9190e8
ibtyog <piotrhirkyj@gmail.com>
2024-10-30 18:29:40 +0100

    Deploy setup

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import express from 'express';
 import cocktailRouter from './routes/cocktail';
 import ingredientRouter from './routes/ingredient';
 import { db_connect } from './db/database';
-require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
 
 const API_URL = "http://localhost"; // your domain address
 const PORT = 3000; // port for your api
-const DB_URI = process.env.DB_URL
+const DB_URI = process.env.DB_URL;
 // add DB_URI in ./.env
 db_connect(DB_URI);
 app.use(express.json());
